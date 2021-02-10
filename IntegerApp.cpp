7ad0591e15d416816171c6fdb61cff7b5463587c
Dan <skafdan@gmail.com>
2021-02-10 17:25:51 +1300

    testing

--- IntegerApp.cpp
+++ IntegerApp.cpp
@@ -5,13 +5,13 @@
 
 int main()
 {
-    std::string a = "2452345345234123123178";
-    std::string b = "23459023850983290589042";
+    std::string a = "-12";
+    std::string b = "-5";
     std::cout<<"The greatest common divisorof,\n\t"<<a<<"\n\t"<<b<<"\nis,\n\t";
     std::cout<<cosc326::gcd(a,b)<<"\n";
     std::string rationalStr = "2452345345234123123178/23459023850983290589042";
-    //cosc326::Integer x(a);
-    //cosc326::Integer y(b);
-    std::cout << cosc326::Rational(rationalStr) << "\n";
+    cosc326::Integer x(a);
+    cosc326::Integer y(b);
+    std::cout << x % y << "\n";
     return 0;
 }
